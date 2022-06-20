991bcdb61f7fb69ec67448d4e290ce08dab3540e
Сергей <wimble@Air-Sergej.mshome.net>
2022-06-20 12:26:23 +0500

    commit

---
+++ AngryBirdsTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AngryBirds
+{
+	public static class AngryBirdsTask
+	{
+		const double G = 9.8;
+		/// <param name="v">Начальная скорость</param>
+		/// <param name="distance">Расстояние до цели</param>
+		/// <returns>Угол прицеливания в радианах от 0 до Pi/2</returns>
+		public static double FindSightAngle(double v, double distance)
+		{
+			return Math.Asin(Math.Sqrt(distance * G) / Math.Pow(v, 2)) / 2;
+		}
+	}
+}
